4b08751637cea6d306e5110dcfedcb171e5ed045
Nick Leso <nickleso.work@gmail.com>
2023-02-16 14:02:52 +0200

    Add email verification
    - add email verification
    - add sendgrid servise
    - add verification email resending

--- middlewares/authValidation.js
+++ middlewares/authValidation.js
@@ -64,8 +64,31 @@ const addSubscriptionValidation = (req, res, next) => {
   next();
 };
 
+const addEmailValidation = (req, res, next) => {
+  const schema = Joi.object({
+    email: Joi.string()
+      .email({
+        minDomainSegments: 2,
+        tlds: { allow: ["com", "net"] },
+      })
+      .required(),
+  });
+
+  const validationResult = schema.validate(req.body);
+
+  if (validationResult.error) {
+    return res.status(400).json({
+      message: validationResult.error.details,
+      code: 400,
+    });
+  }
+
+  next();
+};
+
 module.exports = {
   addSignupValidation,
   addLoginValidation,
   addSubscriptionValidation,
+  addEmailValidation,
 };
